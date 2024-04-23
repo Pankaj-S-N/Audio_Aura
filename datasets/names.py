8e58787c46b375fc63fb934c1655f4977839b812
CaptainHades007 <arceusnfs@gmail.com>
2024-04-24 02:20:58 +0530

    bozo

---
+++ datasets/names.py
@@ -0,0 +1,5 @@
+DUMMY_DATA = './datasets/data/dummy_data.csv'
+SPOTIFY_DATA = './datasets/data/spotify_data.csv'
+MUSIC_DATA = './datasets/data/music_data.csv'
+COUNTRY_DATA = './datasets/data/country_data.csv'
+TASK5_DATA = './datasets/data/dataset1.csv'
