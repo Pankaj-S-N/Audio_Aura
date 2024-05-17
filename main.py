3ba89d9d6fa737e092b0d630ef13b69a61ad7bdc
Pankaj Nandeshwar <106176661+CaptainHades007@users.noreply.github.com>
2024-05-17 16:45:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,45 +7,29 @@ from datasets import names
 # Function to load data
 @st.cache_data
 def load_data():
-    data = pd.read_csv(names.SPOTIFY_DATA)
+    data = pd.read_csv(names.HARYANA_DATA)
     return data
 
 # Load data
 data = load_data()
 
 # Streamlit webpage title
-st.title('Music Tracks Data Visualization')
+st.title('Haryana model comparison')
 
 # Sidebar for user input features
-st.sidebar.header('Filters')
+# st.sidebar.header('Filters')
 
-# Filter by Genre
-selected_genre = st.sidebar.selectbox('Genre', options=['All'] + list(data['track_genre'].unique()))
-if selected_genre != 'All':
-    data = data[data['track_genre'] == selected_genre]
+fig = px.scatter_mapbox(data, lat="latitude", lon="longitude", hover_data=["latitude", "longitude", "label"],
+                        zoom=10, height=600)
 
-# Filter by Artist
-selected_artist = st.sidebar.selectbox('Artist', options=['All'] + list(data['artists'].unique()))
-if selected_artist != 'All':
-    data = data[data['artists'] == selected_artist]
+# Set marker color based on label
+fig.update_traces(marker=dict(color=data['label'].map({0: 'yellow', 1: 'red'})))
 
-# Filter by Popularity
-popularity_range = st.sidebar.slider('Popularity Range', int(data['popularity'].min()), int(data['popularity'].max()), (50, 100))
-data = data[(data['popularity'] >= popularity_range[0]) & (data['popularity'] <= popularity_range[1])]
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
-# Filter by Explicit Content
-explicit_content = st.sidebar.radio('Explicit Content', options=['All', 'Yes', 'No'])
-if explicit_content == 'Yes':
-    data = data[data['explicit'] == 1]
-elif explicit_content == 'No':
-    data = data[data['explicit'] == 0]
-
-# Main page
-st.header('Track Information')
-st.write('Displaying tracks filtered by selected criteria.')
-
-# Plotting the data
-fig = px.bar(data, x='track_name', y='popularity', hover_data=['artists', 'album_name'], color='popularity')
+# Show the plot
+# fig.show()# Plot similar songs
 st.plotly_chart(fig)
 
 # Run this with `streamlit run your_script_name.py`
